46f31f4a185f045c532301def3ce7774953dee73
Kirill <kirill>
2023-08-17 21:06:37 +0500

    remove dead link to account page from header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,11 +7,11 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 import useSignInModal from "@/hooks/modals/useSignInModal";
 import Button from "@/components/Button";
-import { FaUserAlt } from "react-icons/fa";
-import toast from "react-hot-toast";
 import useSignUpModal from "@/hooks/modals/useSignUpModal";
 
 interface HeaderProps {
@@ -29,7 +29,6 @@ const Header: FC<HeaderProps> = ({ children, className }) => {
 
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
-    // Stop playing songs
 
     if (error) toast.error(error.message);
     else toast.success("Logged out!");
@@ -76,12 +75,9 @@ const Header: FC<HeaderProps> = ({ children, className }) => {
             <Button onClick={handleLogout} className="bg-white px-6 py-2">
               Logout
             </Button>
-            <Button
-              onClick={() => router.push("/account")}
-              className="bg-white"
-            >
+            <div className="w-fit rounded-full bg-white px-3 py-3 text-black">
               <FaUserAlt />
-            </Button>
+            </div>
           </div>
         ) : (
           <div className="flex items-center justify-between gap-x-4">
